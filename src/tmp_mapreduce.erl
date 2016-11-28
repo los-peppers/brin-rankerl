87fd34efc74b9ff4130b368c68a21ffd56dd3d4b
Javier Porras <javier.porras@singularities.com>
2016-11-27 19:02:15 -0600

    First example map reduce functions

--- src/tmp_mapreduce.erl
+++ src/tmp_mapreduce.erl
@@ -1,18 +1,8 @@
-%%%-------------------------------------------------------------------
-%%% @author aleph
-%%% @copyright (C) 2016, <COMPANY>
-%%% @doc
-%%%
-%%% @end
-%%% Created : 17. Nov 2016 10:41 PM
-%%%-------------------------------------------------------------------
 -module(tmp_mapreduce).
--author("aleph").
 
--behaviour(gen_server).
 
 %% API
--export([]).
+-export([doMap/4,test/0]).
 
 -record(node,{
   source,
@@ -23,28 +13,65 @@
 %%%%%%%%%%%%%%%%%%%%
 %%%%%MAP%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%
+
+test()->
+  doMap({123,456},[0.25,0.25,0.25,0.25],0,4).
+
+readMtx(_FilePath) ->
+%%    io:format("~p",[FilePath]),
+  [
+    #node{source=1,degree=3,destinations=[2,3,4]},
+    #node{source=2,degree=2,destinations=[1,4]},
+    #node{source=3,degree=1,destinations=[1]},
+    #node{source=4,degree=2,destinations=[2,3]}
+  ].
+
 doMap({ChunkId,ColId},VectorChunk,Beta,K) ->
   FilePath = parseFilePath(ChunkId,ColId),
-  MatrixChunk = readMatrixChunk(FilePath),
-  NewVectorChunk = operateVector(MatrixChunk,VectorChunk,Beta,K),
-  NewVectorChunk. %[{RowId,Summed}...]
+  MatrixChunk = readMtx(FilePath),
+  doNTimes(VectorChunk,{MatrixChunk,Beta,K},50).
+
+doNTimes(Vector,_Params,0)->Vector;
+doNTimes(Vector,Params = {MatrixChunk,Beta,K},N)->
+  io:format("N ~p~n", [N]),
+  NewVector = operateVector(MatrixChunk,Vector,Beta,K),
+  io:format("~p~n", [NewVector]),
+  doNTimes(NewVector,Params,N-1).
+
 
 parseFilePath(_X,_Y) -> "sdfasdf".
 
-readMatrixChunk(_FilePath) ->
-  [
-    node#{source=1,degree=3,destinations=[2,3,4]},
-    node#{source=2,degree=3,destinations=[1,4]},
-    node#{source=3,degree=3,destinations=[3]},
-    node#{source=4,degree=3,destinations=[2,4]}
-  ].
 
-operateVector(MatrixChunk,_VectorChunk,_Beta,_K) ->
-  io:format("Matrix", [MatrixChunk]).
+hasVal([E|_],Num) when E==Num ->
+  true;
+hasVal([E|Rest],Num) when E =/= Num ->
+  hasVal(Rest,Num);
+hasVal([],_) ->
+  false.
+
+getOrZero(#node{degree=Dg,destinations=Dest},I) ->
+  case hasVal(Dest,I) of
+    true -> Dg;
+    false -> 0
+  end.
+
+%TODO: see if K shoud be the matrix size.
+operateVector(MatrixChunk,VectorChunk,_Beta,_K) ->
+  lists:map(fun(I)->
+    Row = [getOrZero(Node,I) || Node <- MatrixChunk],
+    Zipped = lists:zip(VectorChunk,Row),
+    Mapped = lists:map(fun({Vi,Dg})->
+      case Dg of
+        0 -> 0;
+        _ -> Vi/Dg %TODO: enter full formula here.
+      end
+                       end,Zipped),
+    lists:sum(Mapped)%Assuming elements are in order.
+            end,lists:seq(1,_K)).
 %%  operateVector(MatrixChunk,VectorChunk,Beta,[]);
 %%operateVector([Row|Rest],VectorChunk,Beta,Acc) ->
-  %Result = VECTOR * ROW WITH THAT RANDOM SHIT IN THE FORMULA
-  %operateVector(Rest,VectorChunk,Beta,[Result|Acc]);
+%Result = VECTOR * ROW WITH THAT RANDOM SHIT IN THE FORMULA
+%operateVector(Rest,VectorChunk,Beta,[Result|Acc]);
 %%  operateVector([],_VectorChunk,Beta,Acc) ->
 %%lists:reverse(Acc).
 
@@ -52,6 +79,6 @@ operateVector(MatrixChunk,_VectorChunk,_Beta,_K) ->
 %%%%%REDUCE%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%
 %Chunk step list is the result of partial sums for each of the row values
-doReduce({RowId,ChunkStepList}) ->
-  Summed = lists:sum(ChunkStepList),
-  {RowId,Summed}.
+% doReduce({RowId,ChunkStepList}) ->
+%   Summed = lists:sum(ChunkStepList),
+%   {RowId,Summed}.
