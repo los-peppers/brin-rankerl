30531a41aafedd614f540c9a7b988726fd91ee8c
José Marín <jose.marin@singularities.com>
2016-11-29 01:01:17 -0600

    Fix zero bytes chunks bug (maybe, also added an awesome easteregg)

--- src/brin_io.erl
+++ src/brin_io.erl
@@ -1,16 +1,6 @@
 -module(brin_io).
--export([
-  read_file/2]).
+-export([read_file/2]).
 
--record(site, {
-  source :: integer(),
-  degree :: integer(),
-  destinations :: list()
-}).
-
-
-% K = Executors
-%
 -spec read_file(FileName :: string(), Executors :: integer()) -> ok.
 read_file(FileName, Executors) ->
   {ok, IoDevice} = file:open(FileName, [read]),
@@ -19,8 +9,6 @@ read_file(FileName, Executors) ->
   create_chunks(IoDevice, Executors, ChunkSize, NumSites),
   file:close(IoDevice).
 
-
-
 read_num_sites(IoDevice) ->
   case io:get_line(IoDevice, "") of
     eof -> {error, invalid_input_file};
@@ -28,9 +16,6 @@ read_num_sites(IoDevice) ->
       {NumSites, _} = string:to_integer(Line),
       NumSites
   end.
-
-
-
 
 create_chunks(IoDevice, Executors, ChunkSize, NumSites) ->
   do_create_chunks(IoDevice, 0, Executors, ChunkSize, NumSites).
@@ -43,9 +28,6 @@ do_create_chunks(IoDevice, ChunkBlock, Executors, ChunkSize, NumSites) when Chun
 do_create_chunks(_IoDevice, ChunkBlock, Executors, _ChunkSize, _NumSites) when ChunkBlock =:= Executors ->
   ok.
 
-
-
-
 open_chunk_devices(Id, Executors) ->
   do_open_chunk_devices(Id, Executors, []).
 
@@ -53,12 +35,8 @@ do_open_chunk_devices(_Id, 0, Devices) ->
   lists:reverse(Devices);
 do_open_chunk_devices(Id, NumDevices, Devices) ->
   FileName = lists:concat(["/tmp/brio/", integer_to_list(Id), ".pr"]),
-  IoDevice = file:open(FileName, [write]),
+  {ok, IoDevice} = file:open(FileName, [write]),
   do_open_chunk_devices(Id + 1, NumDevices - 1, [IoDevice | Devices]).
-
-
-
-
 
 create_chunk_block(IoDevice, ChunkDevices, ChunkSize, NumSites) ->
   do_create_chunk_block(IoDevice, ChunkDevices, 0, ChunkSize, NumSites).
@@ -79,61 +57,47 @@ do_create_chunk_block(_IoDevice, ChunkDevices, _BlockLines, _ChunkSize, _NumSite
 line_to_list(Line) ->
   [begin {Int, _} = string:to_integer(Token), Int end || Token <- string:tokens(Line, " \n")].
 
-
-
 add_block_line(Neighbors, ChunkDevices, ChunkSize, NumSites) ->
   NumNeighbors = length(Neighbors),
   write_degree(NumNeighbors, ChunkDevices),
-  do_add_block_line(Neighbors, ChunkDevices, 0, ChunkSize, ChunkSize, NumSites, false).
+  do_add_block_line(Neighbors, ChunkDevices, 0, ChunkSize, ChunkSize, NumSites).
 
-do_add_block_line([], [ChunkDevice | ChunkDevices], _Left, _Right, _ChunkSize, _NumSites, Written) ->
-  case Written of
-    true -> write_empty(ChunkDevices);
-    _ -> write_empty([ChunkDevice | ChunkDevices])
-  end,
-  ok;
-do_add_block_line([Neighbor | Neighbors], [ChunkDevice | ChunkDevices], Left, Right, ChunkSize, NumSites, Written) when Left < NumSites ->
+do_add_block_line([], ChunkDevices, _Left, _Right, _ChunkSize, _NumSites) ->
+  write_empty(ChunkDevices);
+do_add_block_line([Neighbor | Neighbors], [ChunkDevice | ChunkDevices], Left, Right, ChunkSize, NumSites) when Left < NumSites ->
   if
     Neighbor >= Right ->
-      if Written =:= false ->
-        write_empty([ChunkDevice]);
-        true -> ok
-      end,
-      do_add_block_line([Neighbor | Neighbors], ChunkDevices, Right, Right + ChunkSize, ChunkSize, NumSites, false);
+      write_empty([ChunkDevice]),
+      do_add_block_line([Neighbor | Neighbors], ChunkDevices, Right, Right + ChunkSize, ChunkSize, NumSites);
     Neighbor >= Left andalso Neighbor < Right ->
       write_neighbor(Neighbor, ChunkDevice),
-      do_add_block_line(Neighbors, [ChunkDevice | ChunkDevices], Left, Right, ChunkSize, NumSites, true)
+      do_add_block_line(Neighbors, [ChunkDevice | ChunkDevices], Left, Right, ChunkSize, NumSites)
   end.
-
-
-
 
 write_neighbor(Neighbor, Device) ->
   Separator = " ",
   file:write(Device, [integer_to_list(Neighbor), Separator]).
 
-
-
-
 write_degree(_Degree, []) ->
   ok;
 write_degree(Degree, [Device | Devices]) ->
-  NewLine = io_lib:nl(),
-  file:write(Device, [Degree, NewLine]),
+  DegreeString = integer_to_list(Degree),
+  write_line(Device, DegreeString),
   write_degree(Degree, Devices).
-
-
 
 write_empty([]) -> ok;
 write_empty([Device | Devices]) ->
-  NewLine = io_lib:nl(),
-  file:write(Device, [NewLine]),
+  write_line(Device, ""),
   write_empty(Devices).
-
-
 
 close_chunks([]) ->
   ok;
 close_chunks([Device | Devices]) ->
+  io:format(Device, "~s", ["Jhoel C'La"]),
   file:close(Device),
   close_chunks(Devices).
+
+write_line(Device, Line) -> do_write_line(Device, Line, os:type()).
+
+do_write_line(Device, Line, _) ->
+  io:format(Device, "~s~n", [Line]).
